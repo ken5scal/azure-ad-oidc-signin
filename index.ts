95ab3072e22018c4318671de630c4d437b5a8491
Kengo Suzuki <kengoscal@gmail.com>
2020-07-19 22:24:35 +0900

    started very rough server

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("hello world")
+import Express from 'express'
+
+const app = Express()
+app.get('/', (req, res) => {
+    const data = { message: 'pong' }
+    res.send(data)
+})
+
+const port = 3000
+const host = 'localhost'
+
+app.listen(port, host, () => {
+    console.log(`Running on http://${host}:${port}`)
+})
